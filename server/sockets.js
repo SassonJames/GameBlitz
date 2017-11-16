ef85f761bc7cc46cac7051323c42a470fb3a1ffe
Jamie Sasson <sassonjames@gmail.com>
2017-11-16 13:32:50 -0500

    Fixed current eslint errors by commenting them out. Added example game to index.html.

--- server/sockets.js
+++ server/sockets.js
@@ -1,23 +1,23 @@
 // fast hashing library
-const xxh = require('xxhashjs');
+// const xxh = require('xxhashjs');
 // node built-in module to start separate processes
 // managed by this node process. This means they will
 // have separate memory and processing (can run across processor cores)
-//const child = require('child_process');
+// const child = require('child_process');
 // Character custom class
-//const Character = require('./messages/Character.js');
+// const Character = require('./messages/Character.js');
 // Custom message class for sending messages to our other process
-//const Message = require('./messages/Message.js');
+// const Message = require('./messages/Message.js');
 
 // object to hold user character objects
-//const charList = {};
+// const charList = {};
 
-//socketio server instance
+// socketio server instance
 let io;
 
-const roomList = {};
-let currentRoom = 0;
-let currentRoomCount = 0;
+// const roomList = {};
+// const currentRoom = 0;
+// const currentRoomCount = 0;
 
 // function to setup our socket server
 const setupSockets = (ioServer) => {
